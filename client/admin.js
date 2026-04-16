e8048b30acfc1e85c394b46ee9e7cbcae4b5f6e3
bashkimhashani <bashkim.hashani@student.uni-pr.edu>
2026-04-16 20:47:23 +0200

    loging code added

--- client/admin.js
+++ client/admin.js
@@ -8,6 +8,21 @@ const CLIENT_PORT = 41235;
 const client = dgram.createSocket("udp4");
 
 function send(msg) {
-const buf = Buffer.from(msg);
-client.send(buf, 0, buf.length, SERVER_PORT, SERVER_IP);
+    const buf = Buffer.from(msg);
+    client.send(buf, 0, buf.length, SERVER_PORT, SERVER_IP);
 }
+
+client.on("message", (msg) => {
+    console.log(`\n[SERVER] ${msg.toString()}\n`);
+    rl.prompt();
+});
+
+client.bind(CLIENT_PORT, () => {
+    console.log("================================");
+    console.log(" ADMIN CLIENT");
+    console.log(" /list | /read | /upload");
+    console.log(" /download | /delete");
+    console.log(" /search | /info | exit");
+    console.log("================================\n");
+    send("HELLO:admin");
+});
